9649a1862f557da6ac6be8f4649cfffd61b7c3dc
Krishantchauhan <krashantchauhan@gmail.com>
2024-11-17 11:04:11 +0530

    all good till now break point
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,39 +1,3 @@
-// import React from 'react';
-// import { FaGithub, FaLinkedin } from 'react-icons/fa';
-
-// function Header() {
-//   return (
-//     <header className="fixed top-0 left-0 w-full z-10 flex justify-between items-center p-6 bg-[#1A0B2E] border-b-2 border-purple-600 rounded-lg">
-//       <div className="flex justify-between w-full">
-//         <div className="flex space-x-4 ">
-//           <a href="https://github.com/Krishantchauhan">
-//             <FaGithub className="hover:scale-125 duration-200" size={40} />
-//           </a>
-//           <a href="https://www.linkedin.com/in/krishant-chauhan/">
-//             <FaLinkedin className="hover:scale-125 duration-200" size={40} />
-//           </a>
-//           <a href="https://leetcode.com/u/krishantchauhan/">
-//             <button className="border rounded-lg px-3 py-2 hover:scale-110 duration-200 hover:bg-[#42256b] border-purple-600">Leetcode</button>
-//           </a>
-//           <a href="https://auth.geeksforgeeks.org/user/krishantchauhan">
-//             <button className="border rounded-lg px-3 py-2 hover:scale-110 duration-200 hover:bg-[#42256b] border-purple-600">GFG</button>
-//           </a>
-//         </div>
-//         <div className="flex space-x-10 mr-64 text-xl pt-2">
-//           <h1>krishant.work@gmail.com</h1>
-//         </div>
-//         <div>
-//           <a>
-//             <button className="border rounded-lg px-3 py-2 hover:scale-110 duration-200 hover:bg-[#42256b] border-purple-600">Resume</button>
-//           </a>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// }
-
-// export default Header;
-
 import React, { useState } from 'react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiMenu, HiX } from 'react-icons/hi'; // Hamburger and close icons
--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -1,23 +1,3 @@
-// import React from 'react';
-
-// function HeroSection() {
-//   return (
-//     <section className="flex flex-col md:flex-row h-screen pt-40 block-bg-hero px-10">
-//       <div className="hidden sm:block md:px-60 mb-20 ">
-//         <div className="purple-gradiant pr-9">
-//           <img src="/src/assets/avatar.png" alt="Profile Avatar" className="w-52 h-52 mb-6 ml-20 " />
-//         </div>
-//       </div>
-//       <div className="mt-8 md:mt-0 md:ml-10 ">
-//         <h2 className="text-3xl mt-7">I'm Krishant Chauhan.</h2>
-//         <p className="text-xl mt-4 py-7 px-14 ">A Software Engineer | BCA & MCA Graduate, building delightful user experiences.</p>
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default HeroSection;
-
 import React from 'react';
 import { motion } from 'framer-motion';
 import useTypingAnimation from './TypingAnimation';
